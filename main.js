9216c4c3db0a02789e3ef16f7f87838946653014
Alonso del Arte <alonso.delarte@gmail.com>
2024-05-21 01:55:52 -0400

    More groundwork

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+    // The following with help from 
+    // https://stackoverflow.com/questions/12070631/how-to-use-json-file-in-html-code
     function appendData(data) {
         let mainContainer = document.getElementById("movieData");
         for (let i = 0; i < data.length; i++) {
