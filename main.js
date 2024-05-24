f256a8aab5e1d40b7b65260b14dd1f9fe77302d2
Alonso del Arte <alonso.delarte@gmail.com>
2024-05-24 15:47:06 -0400

    Added movie poster field

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
-    // The following with help from 
-    // https://stackoverflow.com/questions/12070631/how-to-use-json-file-in-html-code
+// The following with help from 
+// https://stackoverflow.com/questions/12070631/how-to-use-json-file-in-html-code
     function appendData(data) {
         let mainContainer = document.getElementById("movieData");
         for (let i = 0; i < data.length; i++) {
             let div = document.createElement("div");
+            let poster = '<img class="poster" src="' + data[i].posterURL + 
+                '"/>';
             let title = '<h2 class="movieTitle">' + data[i].movieTitle + '</h2>';
             let director = '<li>Director: ' + data[i].director + '</li>';
             let writer = '<li>Writer: ' + data[i].writer + '</li>';
@@ -13,7 +15,8 @@
             let sceneDescription = 
                 '<p class="sceneDesc">Draw the scene in which: ' + data[i].scene + 
                 '</p>'
-            div.innerHTML = title + '<ul>' + director + writer + imdbPageLink + 
+            div.innerHTML = poster + 
+                title + '<ul>' + director + writer + imdbPageLink + 
                 '</ul>' + sceneDescription + '<hr />';
             mainContainer.appendChild(div);
         }
