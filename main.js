944478d250e896d0155f3ddfac199cf5889caaef
Alonso del Arte <alonso.delarte@gmail.com>
2024-05-24 01:44:06 -0400

    Rewrote to display more fields

--- main.js
+++ main.js
@@ -4,7 +4,17 @@
         let mainContainer = document.getElementById("movieData");
         for (let i = 0; i < data.length; i++) {
             let div = document.createElement("div");
-            div.innerHTML = '<h2 class="movieTitle">' + data[i].movieTitle + '</h2>';
+            let title = '<h2 class="movieTitle">' + data[i].movieTitle + '</h2>';
+            let director = '<li>Director: ' + data[i].director + '</li>';
+            let writer = '<li>Writer: ' + data[i].writer + '</li>';
+            let stars = '<li>' + data[i].stars + '</li>';
+            let imdbPageLink = '<li><a href="https://www.imdb.com/title/' + 
+                data[i].imdbID + '/">IMDb page</a></li>';
+            let sceneDescription = 
+                '<p class="sceneDesc">Draw the scene in which: ' + data[i].scene + 
+                '</p>'
+            div.innerHTML = title + '<ul>' + director + writer + imdbPageLink + 
+                '</ul>' + sceneDescription + '<hr />';
             mainContainer.appendChild(div);
         }
     }
